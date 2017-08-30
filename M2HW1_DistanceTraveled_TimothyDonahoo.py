d4365c9a4b3ae5adb5d7cbcb91373ae8d2bf327e
donahoot2109 <31407679+donahoot2109@users.noreply.github.com>
2017-08-30 16:50:54 -0400

    Add files via upload
    Chapter 2, W2HW1, M2HW2

---
+++ M2HW1_DistanceTraveled_TimothyDonahoo.py
@@ -0,0 +1,24 @@
+# CTI-110
+# M2HW1 - Distance Traveled
+# Timothy Donahoo
+# 30AUG17
+
+# The distance the car will travel in 6 hours.
+speed = 70
+print('your speed is', speed, 'miles per hour')
+time1 = 6
+miles1 = 70*6
+print('your total distance traveled in 6 hours is', miles1, 'miles')
+
+# The distance the car will travel in 10 hours.
+time2 = 10
+miles2 = 70*int(time2)
+print ('your total distance traveled in 10 hours is', miles2, 'miles')
+
+# The distance the car will travel in 15 hours.
+time3 = 15
+miles3 = 70*int(time3)
+print ('your total distance traveled in 15 hours is', miles3, 'miles')
+
+
+
